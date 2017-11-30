d945f08cb48461a7523234d4a19ab3effe1b5913
Yann Braga <yannbf@gmail.com>
2017-11-30 08:05:09 -0300

    test(app): remove unused test, add home component import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 describe('AppComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [AppComponent],
+        declarations: [AppComponent, HomeComponent],
       }).compileComponents();
     })
   );
@@ -22,17 +23,6 @@ describe('AppComponent', () => {
       const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.debugElement.componentInstance;
       expect(app.title).toEqual('Real Estatium');
-    })
-  );
-  it(
-    'should render title in a h1 tag',
-    async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain(
-        'Welcome to app!'
-      );
     })
   );
 });
