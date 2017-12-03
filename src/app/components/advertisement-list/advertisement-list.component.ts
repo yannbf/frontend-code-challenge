0a1fec903ad9bd50c17d583bb6eb89099b39a649
Yann Braga <yannbf@gmail.com>
2017-12-03 16:20:38 -0300

    feat(advertisement-list): improve performance with changeDetection strategy

--- src/app/components/advertisement-list/advertisement-list.component.ts
+++ src/app/components/advertisement-list/advertisement-list.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-advertisement-list',
   templateUrl: './advertisement-list.component.html',
   styleUrls: ['./advertisement-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdvertisementListComponent implements OnInit {
   @Input() advertisements;
