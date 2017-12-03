1290d70ddc28f414369feb24a908a9344ecd4669
Yann Braga <yannbf@gmail.com>
2017-12-03 16:19:09 -0300

    test(advertisement-service): add tests

--- src/app/services/advertisement.service.spec.ts
+++ src/app/services/advertisement.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AdvertisementService } from './advertisement.service';
+import { AdvertisementServiceMock } from './mock/advertisement.service.mock';
 
 describe('AdvertisementService', () => {
+  let advertisementService: AdvertisementService;
+  let httpMock: HttpTestingController;
+  const endpoint = 'https://api.mcmakler.de/v1/advertisements';
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [AdvertisementService],
     });
+
+    advertisementService = TestBed.get(AdvertisementService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it(
@@ -15,4 +28,22 @@ describe('AdvertisementService', () => {
       expect(service).toBeTruthy();
     })
   );
+
+  describe('#getAdvertisements', () => {
+    afterEach(() => {
+      httpMock.verify();
+    });
+
+    it('should return a list of 15 advertisements', () => {
+      const mockAdvertisements = AdvertisementServiceMock.ADVERTISEMENTS;
+
+      advertisementService.getAdvertisements().subscribe(coins => {
+        expect(coins.length).toBe(15);
+      });
+
+      const req = httpMock.expectOne(endpoint);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockAdvertisements);
+    });
+  });
 });
