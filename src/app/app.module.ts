e6e6de52358deccf083bbe63aadce0b3188dc8ed
Yann Braga <yannbf@gmail.com>
2017-12-03 12:52:11 -0300

    feat(ngrx): register advertisement reducer to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { advertisementReducer } from './state/reducers';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ advertisements: advertisementReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
