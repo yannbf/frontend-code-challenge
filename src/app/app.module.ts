4360853f4dbe6b763935f3dad2162cf89eada9db
Yann Braga <yannbf@gmail.com>
2017-12-03 13:31:05 -0300

    feat(service): add advertisement service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { advertisementReducer } from './state/reducers';
+import { AdvertisementService } from './services';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     StoreModule.forRoot({ advertisements: advertisementReducer }),
   ],
-  providers: [],
+  providers: [AdvertisementService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
