8b1dafbd1a97f2ef5257caa2f10fcebc6ee2a631
Yann Braga <yannbf@gmail.com>
2017-12-05 19:39:07 -0300

    feat(adv-reducer): add isLoading state

--- src/app/state/reducers/advertisement.reducer.ts
+++ src/app/state/reducers/advertisement.reducer.ts
@@ -4,6 +4,7 @@ import { Advertisement } from '../../shared/models';
 export interface AdvertisementState {
   error: any;
   advertisements: Array<Advertisement>;
+  isLoading: boolean;
 }
 
 export function advertisementReducer(
@@ -11,16 +12,27 @@ export function advertisementReducer(
   action: AdvertisementActions
 ): AdvertisementState {
   switch (action.type) {
+    case AdvertisementActionTypes.LOAD_ADVERTISEMENTS: {
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+    }
+
     case AdvertisementActionTypes.LOAD_ADVERTISEMENTS_SUCCESS: {
       return {
         ...state,
         advertisements: action.payload,
+        isLoading: false,
+        error: null,
       };
     }
 
     case AdvertisementActionTypes.LOAD_ADVERTISEMENTS_FAIL: {
       return {
         ...state,
+        isLoading: false,
         error: action.payload,
       };
     }
